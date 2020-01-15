53645e3823d560076082cca2c27d0bffb79e9888
Clive Da <cliveda@example.com>
2020-01-15 19:57:26 +0000

    touched

---
+++ index.js
@@ -0,0 +1,19 @@
+const express = require('express')
+
+const app = express()
+
+app.get('*', (req, res) => {
+    res.set('Content-Type', 'text/html')
+    res.status(200).send(`
+        <h1>where can we go today</h1>
+        <h2>visit <a href="https://www.visitsurrey.com/">surrey</a></h2>
+        <h2>visit <a href="https://www.visitsurrey.com/explore/towns">surrey map</a></h2>
+        <h2>visit <a href="https://www.visitsoutheastengland.com/places-to-visit/sussex">sussex</a></h2>
+        <h2>visit <a href="https://www.visitsoutheastengland.com/places-to-visit/sussex/map">sussex map</a></h2>
+    `)
+})
+
+module.exports = app
+
+if (process.env.LOCALRUN) app.listen(3000)
+
